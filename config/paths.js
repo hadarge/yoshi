a93a6ab1b54e28e64caffce42b6bcd6ef362c9db
Ran Yitzhaki <ranyitzhaki@gmail.com>
2017-10-18 18:13:24 +0300

    wip - initial configuration and wiring

---
+++ config/paths.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+const appDirectory = fs.realpathSync(process.cwd());
+
+module.exports = {
+  appDirectory,
+  build: 'dist',
+  target: 'target',
+  assets: 'assets',
+  src: 'src',
+  test: 'test',
+  entry: './src/app.js',
+  config: {
+    webpack: {
+      development: require.resolve('./webpack.config.development'),
+      production: require.resolve('./webpack.config.production')
+    }
+  }
+};
---
+++ src/commands/build.js
@@ -0,0 +1,32 @@
+const LoggerPlugin = require('haste-plugin-logger');
+const paths = require('../../config/paths');
+
+module.exports = async (configure) => {
+  const { run, tasks } = configure({
+    plugins: [
+      new LoggerPlugin(),
+    ],
+  });
+
+  const { clean, read, babel, write, sass, webpack } = tasks;
+
+  await run(clean({ pattern: `${paths.build}/*` }));
+
+  // await Promise.all([
+  //   run(
+  //     read({ pattern: `${paths.src}/**/*.js` }),
+  //     babel(),
+  //     write({ target: paths.build })
+  //   ),
+  //   run(
+  //     read({ pattern: `${paths.src}/**/*.scss` }),
+  //     sass(),
+  //     write({ target: paths.build })
+  //   ),
+  //   run(
+  //     read({ pattern: `${paths.assets}/**/*.*` }),
+  //     write({ target: paths.build })
+  //   ),
+  //   run(webpack({ configPath: paths.config.webpack.production }))
+  // ]);
+};
