a4067003f6ee8d66401b75ca3a0a8b697868ec88
Ran Yitzhaki <ranyitzhaki@gmail.com>
2017-10-23 15:02:03 +0300

    add haste-plugin-wix-logger

---
+++ haste-plugin-wix-logger/src/index.js
@@ -0,0 +1,31 @@
+const chalk = require('chalk');
+const { format, delta, generateRunTitle } = require('./utils');
+
+module.exports = class LoggerPlugin {
+  apply(runner) {
+    runner.plugin('start-worker', (worker) => {
+      ['stdout', 'stderr'].forEach(name => worker.child[name].pipe(process[name]));
+    });
+
+    runner.plugin('start-run', (runPhase) => {
+      if (runner.idle) {
+        return;
+      }
+
+      const runTitle = generateRunTitle(runPhase.tasks);
+
+      const start = new Date();
+      console.log(`[${format(start)}] ${chalk.black.bgGreen('Starting')} '${runTitle}'...`);
+
+      runPhase.plugin('succeed-run', () => {
+        const [end, time] = delta(start);
+        console.log(`[${format(end)}] ${chalk.black.bgCyan('Finished')} '${runTitle}' after ${time} ms`);
+      });
+
+      runPhase.plugin('failed-run', () => {
+        const [end, time] = delta(start);
+        console.log(`[${format(end)}] ${chalk.white.bgRed('Failed')} '${runTitle}' after ${time} ms`);
+      });
+    });
+  }
+};
