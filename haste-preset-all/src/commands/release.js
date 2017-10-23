7c4fbd27128e91657020b96ea716dd2f12cfc213
Ran Yitzhaki <ranyitzhaki@gmail.com>
2017-10-23 17:07:07 +0300

    add release command

---
+++ haste-preset-all/src/commands/release.js
@@ -0,0 +1,13 @@
+const LoggerPlugin = require('haste-plugin-wix-logger');
+
+module.exports = async (configure) => {
+  const { run, tasks } = configure({
+    plugins: [
+      new LoggerPlugin(),
+    ],
+  });
+
+  const { wnpmRelease } = tasks;
+
+  await run(wnpmRelease());
+};
--- haste-preset-all/src/index.js
+++ haste-preset-all/src/index.js
@@ -2,10 +2,12 @@ const build = require('./commands/build');
 const start = require('./commands/start');
 const lint = require('./commands/lint');
 const test = require('./commands/test');
+const release = require('./commands/release');
 
 module.exports = {
   build,
   start,
   lint,
   test,
+  release,
 };
