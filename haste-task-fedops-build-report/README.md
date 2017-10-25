505a912ae76068aab0d72c823bd716bc7947c426
Ran Yitzhaki <ranyitz@users.noreply.github.com>
2017-10-25 17:58:01 +0300

    haste-task-fedops-build-report (#8)

---
+++ haste-task-fedops-build-report/README.md
@@ -0,0 +1,4 @@
+# haste-task-fedops-build-report
+
+The task accepts an object with the following arguments
+* **base `<string>`** path to the root of the project (default to `process.cwd()`)
---
+++ haste-task-fedops-build-report/src/index.js
@@ -0,0 +1,116 @@
+const path = require('path');
+const graphite = require('graphite-tcp');
+const fs = require('fs');
+const glob = require('glob');
+
+const fsStatP = filePath => new Promise((resolve, reject) => {
+  fs.stat(filePath, (err, stats) => {
+    if (err) {
+      reject(err);
+    } else {
+      resolve(stats);
+    }
+  });
+});
+
+const tryRequire = name => {
+  try {
+    return require(name);
+  } catch (ex) {
+    return null;
+  }
+};
+
+const globAsync = (patter, options) => new Promise((resolve, reject) => {
+  glob(patter, options, (err, matches) => {
+    if (err) {
+      reject(err);
+      return;
+    }
+
+    resolve(matches);
+  });
+});
+
+
+module.exports = ({base = process.cwd()}) => async () => {
+  const getBundleNames = () => {
+    return globAsync(path.resolve(base, 'dist/statics/*.min.@(js|css)'));
+  };
+
+  const replaceDotsWithUnderscore = str => str.replace(/\./g, '_');
+
+  const command = ({appName, bundleName}) => {
+    const metricNames = {
+      app_name: replaceDotsWithUnderscore(appName), // eslint-disable-line camelcase
+      bundle_name: replaceDotsWithUnderscore(path.relative(path.join(base, 'dist/statics'), bundleName)), // eslint-disable-line camelcase
+    };
+
+    const metricNamesSeparator = '.';
+    const bundleSizeMetricName = 'bundle_size';
+
+    return ''.concat(
+      Object.keys(metricNames).map(key => `${key}=${metricNames[key]}`).join(metricNamesSeparator),
+      metricNamesSeparator,
+      `${bundleSizeMetricName}`
+    );
+  };
+
+  const reportBundleSize = params => {
+    return new Promise(resolve => {
+      return fsStatP(path.resolve(base, params.bundleName))
+        .then(stats => {
+          const metric = graphite.createClient({
+            host: 'm.wixpress.com',
+            port: 2003,
+            prefix: 'wix-bi-tube.root=events_catalog.src=72',
+            callback: () => {
+              resolve();
+              metric.close();
+            }
+          });
+          console.log(command(params), stats.size || 0);
+          metric.put(command(params), stats.size || 0);
+        })
+        .catch(err => {
+          console.warn(`Error code ${err.code}. Failed to find size of file ${params.bundleName}.bundle.min.js.`);
+          resolve();
+        });
+    });
+  };
+
+  const reportBundleForApp = bundleName => fedopsJson => {
+    const appName = fedopsJson.app_name || fedopsJson.appName;
+    const params = {
+      appName,
+      bundleName
+    };
+
+    if (!appName) {
+      console.warn('fedops.json is missing "app_name" field');
+      return Promise.resolve();
+    }
+
+    return reportBundleSize(params);
+  };
+
+  const sendStream = config => bundleName => {
+    const promises = [].concat(config).map(reportBundleForApp(bundleName));
+    return Promise.all(promises);
+  };
+
+  const config = tryRequire(path.join(base, 'fedops.json'));
+
+  if (!config) {
+    return Promise.resolve();
+  }
+
+  return getBundleNames()
+    .then(bundleNames => {
+      return Promise.all(bundleNames.map(sendStream(config)));
+    })
+    .catch(e => {
+      console.warn('Bundle size report error:', e);
+      return Promise.resolve();
+    });
+};
