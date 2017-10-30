4bdea7fe6bbbb4c63e89f67bc10ba19f4efea8ae
Ronen Amiel <ronen.amiel@gmail.com>
2017-10-30 16:46:30 +0200

    haste-preset-all: test command watches over changes if WIX_NODE_BUILD_WATCH_MODE env var is defined

--- haste-preset-all/src/commands/test.js
+++ haste-preset-all/src/commands/test.js
@@ -1,12 +1,16 @@
+const path = require('path');
 const LoggerPlugin = require('haste-plugin-wix-logger');
 const globs = require('../globs');
 const {inTeamCity} = require('../utils');
 
+const shouldWatch = process.env.WIX_NODE_BUILD_WATCH_MODE;
+
 module.exports = async configure => {
-  const {run, tasks} = configure({
+  const {run, tasks, watch} = configure({
     plugins: [
       new LoggerPlugin(),
     ],
+    persistent: shouldWatch,
   });
 
   const {read, mocha, protractor} = tasks;
@@ -20,11 +24,24 @@ module.exports = async configure => {
     })
   );
 
-  await run(
-    protractor({
-      webdriverManagerArgs: ['--standalone', '--versions.chrome', '2.29', '--gecko', 'false'],
-      configPath: require.resolve('../../config/protractor.conf.js')
-    })
-  );
+  if (shouldWatch) {
+    watch([globs.specs(), path.join(globs.base(), '**', '*.js{,x}'), 'index.js'], () => run(
+      read({pattern: globs.specs()}),
+      mocha({
+        requireFiles: [require.resolve('../../config/test-setup')],
+        timeout: 30000,
+        reporter: inTeamCity() ? 'mocha-env-reporter' : 'progress',
+      }),
+    ));
+  }
+
+  if (!shouldWatch) {
+    await run(
+      protractor({
+        webdriverManagerArgs: ['--standalone', '--versions.chrome', '2.29', '--gecko', 'false'],
+        configPath: require.resolve('../../config/protractor.conf.js')
+      })
+    );
+  }
 };
 
