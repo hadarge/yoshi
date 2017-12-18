ddaa96e4f6ecae8f87fd5c24c8e28e238fb5c077
Shlomi Toussia-Cohen <shlomitc@gmail.com>
2017-12-18 13:36:13 +0200

    Revert "Revert "remove migrate bower feature flag (#89)" (#98)" (#102)
    This reverts commit 1d5d8bad2b67543f606857163663088695a029fa.

--- haste-preset-yoshi/src/tasks/migrate-bower-artifactory/index.js
+++ haste-preset-yoshi/src/tasks/migrate-bower-artifactory/index.js
@@ -1,7 +1,5 @@
 const {migrate} = require('migrate-bower-artifactory');
 
 module.exports = () => {
-  return process.env.MIGRATE_BOWER_ARTIFACTORY_TOOL ?
-    async () => migrate() :
-    async () => null;
+  return async () => migrate();
 };
