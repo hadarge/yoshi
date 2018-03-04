b0bdf6096c0e82709cb065fe96293a8d6696fa22
Ran Yitzhaki <ranyitz@users.noreply.github.com>
2018-03-04 10:10:41 +0200

    Custom publish script (#169)

--- haste-preset-yoshi/scripts/release.js
+++ haste-preset-yoshi/scripts/release.js
@@ -1,61 +1,87 @@
-// The code was taken from https://github.com/wix/wix-style-react/blob/master/scripts/npm-release.js
-
-if (!process.env.IS_BUILD_AGENT) {
-  console.log('Package will not be published because we\'re not running in a CI build agent');
-  return process.exit(0);
-}
-
-const path = require('path');
+const fs = require('fs');
+const execSync = require('child_process').execSync;
+const chalk = require('chalk');
 const semver = require('semver');
-const exec = promisify(require('child_process').exec);
-const writeFile = promisify(require('fs').writeFile);
+const intersection = require('lodash/intersection');
 
-const packageJSONPath = path.resolve(__dirname, '..', 'package.json');
-const packageJSON = require(packageJSONPath);
+const chosenPackages = process.argv.slice(2);
+const packagesToReleaseFrom = ['haste-preset-yoshi', 'yoshi'];
+const packagesToRelease = intersection(chosenPackages, packagesToReleaseFrom);
 
-const getPublishedVersion = () =>
-  exec('npm view haste-preset-yoshi version --registry=https://registry.npmjs.org/')
-    .catch(e => console.log('ERROR: Unable to get published version from npmjs', e));
+const DEFAULT_REGISTRY = 'https://registry.npmjs.org/';
+const packageJsonPath = require.resolve('../package.json');
+const pkg = require(packageJsonPath);
+const version = pkg.version;
+const publishConfig = pkg.publishConfig || {};
+const registry = publishConfig.registry || DEFAULT_REGISTRY;
+let tag = 'latest';
 
-
-writeFile(
-  packageJSONPath,
-  stringify(Object.assign({}, packageJSON, {private: true}))
-)
-
-  .then(getPublishedVersion)
-
-  .then(publishedVersion =>
-    [
-      semver.gt(packageJSON.version, publishedVersion),
-      packageJSON.version,
-      publishedVersion
-    ]
-  )
-
-  .then(([shouldPublish, packageJSONVersion, publishedVersion]) =>
-    shouldPublish ?
-      writeFile(packageJSONPath, stringify(packageJSON))
-        .then(() => console.log(`Package will be published because version set in package.json ${packageJSONVersion} is newer than published ${publishedVersion}`)) :
-          console.log(`Package will not be published because version ${packageJSONVersion} set in package.json is already published`)
-  )
-
-  .catch(error => {
-    console.error('ERROR: Unable to publish', error);
-    process.exit(1);
-  });
-
-// promisify : Function -> List arguments -> Promise
-function promisify(fn) {
-  return (...args) =>
-    new Promise((resolve, reject) =>
-      fn(
-        ...args,
-        (err, payload) => err ? reject(err) : resolve(payload)
-      )
-    );
+if (semver.prerelease(version) !== null) {
+  tag = 'next';
 }
 
-function stringify(data) {
-  return JSON.stringify(data, null, 2);
+function getPublishedVersions(pkgName, registry) {
+  try {
+    const rawVersions = execSync(`npm show ${pkgName} versions --registry=${registry} --json`);
+    return JSON.parse(rawVersions);
+  } catch (error) {
+    if (error.stderr.toString().includes('npm ERR! code E404')) {
+      console.error(chalk.yellow('\nWarning: package not found. Possibly not published yet'));
+      return [];
+    }
+
+    throw error;
+  }
+}
+
+function shouldPublishPackage(name, version, registry) {
+  const versions = getPublishedVersions(name, registry);
+
+  return !versions.includes(version);
+}
+
+function modifyPackageJson(packageChanges) {
+  const modifiedPkg = JSON.stringify(Object.assign({}, pkg, packageChanges), null, 2);
+  fs.writeFileSync(packageJsonPath, modifiedPkg);
+}
+
+function publish(name) {
+  modifyPackageJson({name});
+
+  const publishCommand = `npm publish --tag=${tag} --registry=${registry}`;
+
+  console.log(chalk.magenta(`running: "${publishCommand}" for ${name}@${version}`));
+
+  execSync(publishCommand);
+}
+
+function release(packagesToRelease) {
+  console.log(`starting the release process for ${packagesToRelease.join(' & ')}\n`);
+  const unpublishablePackages = packagesToRelease.filter(name => !shouldPublishPackage(name, version, registry));
+
+  if (unpublishablePackages.length > 0) {
+    unpublishablePackages.forEach(name => {
+      console.log(chalk.blue(`${name}@${version} is already exist on registry ${registry}`));
+    });
+
+    console.log('\nNo publish performed');
+
+    process.exit(0);
+  }
+
+  packagesToRelease.forEach(publish);
+
+  console.log(chalk.green(`\nPublish ${packagesToRelease.map(name => `"${name}@${version}"`).join(' & ')} succesfully to ${registry}`));
+}
+
+try {
+  // 1. choose a tag -> `next` for prerelease (beta/alpha/rc) and `latest` for release.
+  // 2. verify that all packages can be published by checking the registry.
+  // 3. go over each package name:
+  // 3.1. replace the name in `package.json`.
+  // 3.2. perform npm publish using the chosen tag.
+  release(packagesToRelease);
+} finally {
+  // 4. write back the original `package.json`.
+  modifyPackageJson();
 }
