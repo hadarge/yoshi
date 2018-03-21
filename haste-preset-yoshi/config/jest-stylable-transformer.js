38ed8b4c05d1a95f962fe6e1d24a912920c4f20a
Yarden Sod-Moriah <yardnsm@gmail.com>
2018-03-21 12:06:55 +0200

    Add `stylable-integration` require-hooks and transform functions (#143)

---
+++ haste-preset-yoshi/config/jest-stylable-transformer.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+exports.process = (src, path) => {
+  const fn = require('stylable-integration/dist/src/stylable-to-module-factory').stylableToModuleFactory(fs, require);
+  const result = fn(src, path);
+
+  //This is a temporary patch until stylable-integration will allow passing a path / use absolute path for stylable/runtime
+  return result.replace('stylable/runtime', require.resolve('stylable/runtime'));
+};
