94e543b1edc775591057b49e4561e8f70882f76c
Ran Yitzhaki <ranyitz@gmail.com>
2018-04-08 16:32:48 +0300

    move yoshi-runtime to fed-infra repo

--- yoshi-runtime/utils.js
+++
@@ -1,8 +0,0 @@
-module.exports.isProduction = () => (process.env.NODE_ENV || '').toLowerCase() === 'production';
-
-module.exports.isCI = () => !!(
-  process.env.CONTINUOUS_INTEGRATION ||
-  process.env.BUILD_NUMBER ||
-  process.env.TEAMCITY_VERSION ||
-  false
-);
