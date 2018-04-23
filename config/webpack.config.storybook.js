90bf7981ac3c62bc015b6fd91621bd577acca2ad
Ran Yitzhaki <ranyitz@users.noreply.github.com>
2018-04-23 16:30:23 +0300

    update stylableWebpackPlugin to the storybook config (#216)

--- config/webpack.config.storybook.js
+++ config/webpack.config.storybook.js
@@ -1,8 +1,7 @@
 const union = require('lodash/union');
 const webpackCommonConfig = require('./webpack.config.common');
 const projectConfig = require('./project');
-
-// const stylable = require('../src/loaders/stylable');
+const StylableWebpackPlugin = require('stylable-webpack-plugin');
 
 module.exports = config => {
   const projectName = projectConfig.name();
@@ -17,7 +16,7 @@ module.exports = config => {
 
   config.plugins = [
     ...config.plugins || [],
-    // stylable.plugin()
+    new StylableWebpackPlugin()
   ];
 
   return config;
