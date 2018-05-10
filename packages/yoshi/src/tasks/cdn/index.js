87e08797bfa7262d440beae7a14da78749050a2b
Artem Yavorsky <aqson@me.com>
2018-05-10 12:40:56 +0300

    Fix lint

--- packages/yoshi/src/tasks/cdn/index.js
+++ packages/yoshi/src/tasks/cdn/index.js
@@ -29,7 +29,7 @@ module.exports = ({
       const webpackConfig = getConfig({
         separateCss: false,
         debug: true,
-        disableModuleConcatenation: true
+        disableModuleConcatenation: true,
       });
 
       if (shouldRunWebpack(webpackConfig, defaultEntry, configuredEntry)) {
