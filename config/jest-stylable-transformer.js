82760ee908cfa9dc3a9a1365c58f83ffd5602260
Yair Haimovitch <yairh@wix.com>
2018-05-10 17:31:29 +0300

    fix for require path for windows

--- config/jest-stylable-transformer.js
+++ config/jest-stylable-transformer.js
@@ -4,6 +4,7 @@ exports.process = (src, path) => {
   const fn = require('stylable-integration/dist/src/stylable-to-module-factory').stylableToModuleFactory(fs, require);
   const result = fn(src, path);
 
-  //This is a temporary patch until stylable-integration will allow passing a path / use absolute path for stylable/runtime
-  return result.replace('stylable/runtime', require.resolve('stylable/runtime'));
+  // This is a temporary patch until stylable-integration will allow passing a path / use absolute path for stylable/runtime
+  // Replacing single backslashes to double backslashes in order to support execution on Windows
+  return result.replace('stylable/runtime', require.resolve('stylable/runtime').replace(/\\/g, '\\\\'));
 };
