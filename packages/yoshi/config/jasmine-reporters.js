9c231432d2fb13baed86b57768429dd81e5c82ca
Ran Yitzhaki <ranyitz@users.noreply.github.com>
2018-06-06 14:55:07 +0300

    fix jasmine base reporter (#340)

--- packages/yoshi/config/jasmine-reporters.js
+++
@@ -1,7 +0,0 @@
-const { TerminalReporter, TeamCityReporter } = require('jasmine-reporters');
-const { inTeamCity } = require('../src/utils');
-
-module.exports = [
-  new TerminalReporter({ color: true, verbosity: 2 }),
-  ...(inTeamCity() ? [new TeamCityReporter()] : []),
-];
--- packages/yoshi/config/jasmine-setup.js
+++ packages/yoshi/config/jasmine-setup.js
@@ -1,14 +1,9 @@
 /* eslint-env jasmine */
 
-const { TerminalReporter, TeamCityReporter } = require('jasmine-reporters');
+const { TeamCityReporter } = require('jasmine-reporters');
 const { inTeamCity } = require('../src/utils');
 
-jasmine.getEnv().clearReporters();
-
-jasmine
-  .getEnv()
-  .addReporter(new TerminalReporter({ color: true, verbosity: 2 }));
-
 if (inTeamCity()) {
+  jasmine.getEnv().clearReporters();
   jasmine.getEnv().addReporter(new TeamCityReporter());
 }
