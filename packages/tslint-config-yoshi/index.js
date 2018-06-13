64b2af41ded344d64ff8209a888fcd2318a750f8
Ronen Amiel <ronen.amiel@gmail.com>
2018-06-13 15:00:48 +0300

    fix double comment

--- packages/tslint-config-yoshi/index.js
+++ packages/tslint-config-yoshi/index.js
@@ -4,7 +4,7 @@ module.exports = {
   extends: ['tslint-config-yoshi-base'],
 
   rules: {
-    // // https://github.com/palantir/tslint-react
+    // https://github.com/palantir/tslint-react
     'jsx-no-bind': true,
     'jsx-no-string-ref': true,
   },
