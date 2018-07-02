fb4fa914dd8c3d52f0e796aacad8e6b3cf39ea9c
Ronen Amiel <ronena@wix.com>
2018-07-02 10:49:59 +0300

    Configure jasmine to not run tests randomly (introduced in v3) (#410)
    ### 🔦 Summary
    
    This PR disables randomizing test order that Jasmine 3 configures by default.

--- packages/yoshi/config/jasmine-config.js
+++ packages/yoshi/config/jasmine-config.js
@@ -2,13 +2,12 @@ const globs = require('../src/globs');
 const projectConfig = require('./project');
 
 module.exports = {
-  spec_dir: '', //eslint-disable-line camelcase
-  spec_files: [
-    //eslint-disable-line camelcase
-    projectConfig.specs.node() || globs.specs(),
-  ],
+  spec_dir: '',
+  spec_files: [projectConfig.specs.node() || globs.specs()],
   helpers: [
     require.resolve('./jasmine-setup.js'),
     require.resolve('./test-setup.js'),
   ],
+  stopSpecOnExpectationFailure: false,
+  random: false,
 };
