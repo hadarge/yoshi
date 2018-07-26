82a8320d5ac7c458ffa190ec00b468c6e15f9e16
yairhaimo <yair.haimo@gmail.com>
2018-07-26 10:59:59 +0300

    Add wix-style-react lint rules (#461)
    <!---
    Thanks for submitting a pull request 😄 !
    -->
    
    <!--- If you're changing public APIs make sure to document them (README, FAQ) -->
    
    <!--- Be as descriptive as possible when explaining what was changed. Link to an issue if one exists -->
    ### 🔦 Summary
    Adds the `wix-style-react/no-full-wsr-lib` temporary rule to our lint packages.
    This rule fails when importing `wix-style-react` components in a way that imports the entire library (until wsr supports tree shaking).

--- packages/eslint-config-yoshi/index.js
+++ packages/eslint-config-yoshi/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: require.resolve('eslint-config-yoshi-base'),
 
-  plugins: ['jsx-a11y', 'react'],
+  plugins: ['jsx-a11y', 'react', 'eslint-plugin-wix-style-react'],
 
   env: {
     browser: true,
@@ -65,5 +65,8 @@ module.exports = {
     'jsx-a11y/role-has-required-aria-props': 'warn',
     'jsx-a11y/role-supports-aria-props': 'warn',
     'jsx-a11y/scope': 'warn',
+
+    // https://github.com/wix/wix-ui/tree/master/packages/eslint-plugin-wix-style-react
+    'wix-style-react/no-full-wsr-lib': 'error',
   },
 };
--- packages/tslint-config-yoshi/index.js
+++ packages/tslint-config-yoshi/index.js
@@ -3,7 +3,11 @@ const addJsRules = require('tslint-config-yoshi-base/addJsRules');
 module.exports = addJsRules({
   rulesDirectory: ['tslint-react'],
 
-  extends: ['tslint-react', 'tslint-config-yoshi-base'],
+  extends: [
+    'tslint-react',
+    'tslint-config-yoshi-base',
+    'tslint-plugin-wix-style-react',
+  ],
 
   rules: {
     // https://github.com/palantir/tslint-react
@@ -13,5 +17,8 @@ module.exports = addJsRules({
     'jsx-no-lambda': false,
     'jsx-no-string-ref': true,
     'jsx-self-close': false,
+
+    // https://github.com/wix/wix-ui/tree/master/packages/tslint-plugin-wix-style-react
+    'no-full-wsr-lib': true,
   },
 });
