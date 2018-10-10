a88033d0f76bccc522f16a99fe3aeff493c0a2f3
Netanel Gilad <netanelg@wix.com>
2018-10-10 18:25:15 +0300

    Fix wallaby babel (#623)
    <!---
    Thanks for submitting a pull request 😄 !
    -->
    
    <!--- If you're changing public APIs make sure to document them (README, FAQ) -->
    
    <!--- Be as descriptive as possible when explaining what was changed. Link to an issue if one exists -->
    ### 🔦 Summary
    It seems that wallaby ignores jest `transform` configuration, so we need to add a wallaby compiler for js files. Wallaby = 👎  😉 
    
    <!--- Describe how the proposed changes are tested -->
    ### 🗺️ Test Plan
    N/A

--- packages/yoshi/config/wallaby-jest.js
+++ packages/yoshi/config/wallaby-jest.js
@@ -1,5 +1,13 @@
 module.exports = function(wallaby) {
   const wallabyCommon = require('./wallaby-common')(wallaby);
+  // We need to add a wallaby compiler, because apparently wallaby doesn't
+  // use the transform configuration of jest
+  wallabyCommon.compilers = {
+    '**/*.js{,x}': wallaby.compilers.babel({
+      babel: require('babel-core'), // Make sure wallaby is using babel 6.X
+      babelrc: true,
+    }),
+  };
   wallabyCommon.testFramework = 'jest';
   wallabyCommon.setup = () => {
     let jestConfig = require('yoshi/config/jest.config.js'); // eslint-disable-line import/no-unresolved
