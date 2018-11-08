0f9316f4c506e272356ad96eed4b13aef827619a
Ronen Amiel <ronena@wix.com>
2018-11-08 17:06:00 +0200

    Migrate to ESlint version 5 (#712)

--- packages/eslint-config-yoshi-base/index.js
+++ packages/eslint-config-yoshi-base/index.js
@@ -15,7 +15,6 @@ module.exports = {
     ecmaVersion: 6,
     ecmaFeatures: {
       generators: true,
-      experimentalObjectRestSpread: true,
     },
   },
 
--- packages/eslint-config-yoshi/index.js
+++ packages/eslint-config-yoshi/index.js
@@ -13,7 +13,6 @@ module.exports = {
     ecmaFeatures: {
       jsx: true,
       generators: true,
-      experimentalObjectRestSpread: true,
     },
   },
 
