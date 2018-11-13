5db2f76009c1027dc40856fae897e32e6dd64824
Ronen Amiel <ronena@wix.com>
2018-11-13 11:26:21 +0200

    Add basic types to bootstrap-hot-loader (#773)
    ### 🔦 Summary
    
    Since `bootstrap-hot-loader` is a runtime library, users that work with Typescript expect it to have its own definition file.

---
+++ packages/bootstrap-hot-loader/bootstrap-hot-loader.d.ts
@@ -0,0 +1,8 @@
+declare module 'bootstrap-hot-loader' {
+  import { Router } from 'express';
+
+  export function hot(
+    module: any,
+    wrappedFunction: (router: Router, context?: any) => Router
+  ): (router: Router, context?: any) => Router
+}
