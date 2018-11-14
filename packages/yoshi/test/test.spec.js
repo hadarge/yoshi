hadarge/yoshi

commit 187b8863cdc5143d0e3910cfba27c84dc14943a2

Starting cdn when e2e exists (#775)
### 🔦 Summary
Checking that e2e tests exists for starting cdn.
### 🗺️ Test Plan
Was developed TDD style - wrote a test that broke and then fixed the code

packages/yoshi-config/globs.js
@@ -16,7 +16,8 @@ module.exports = {
   statics,
   babel: [path.join(base, '**', '*.js{,x}'), 'index.js'],
   specs: `${base}/**/*.+(spec|it).+(js|ts){,x}`,
-  e2e: `${test}/**/*.e2e.{js,ts}`,
+  protractor: `${test}/**/*.e2e.{js,ts}`,
+  puppeteer: `${test}/e2e/**/*.spec.(ts|js){,x}`,
   testFilesWatch: [path.join(base, '**', '*.(ts|js){,x}'), 'index.js'],
   singleModule: {
     clientDist: statics,

packages/yoshi-helpers/queries.js
@@ -71,7 +71,7 @@ module.exports.shouldRunLess = () => {
 };
 
 module.exports.hasE2ETests = () => {
-  return glob.sync(globs.e2e).length > 0;
+  return glob.sync(globs.protractor).length > 0;
 };
 
 module.exports.hasProtractorConfigFile = () => {

packages/yoshi/test/test.spec.js
@@ -304,6 +304,11 @@ describe('Aggregator: Test', () => {
         expect(res.stderr).to.contain('5 passed, 5 total');
       });
 
+      it('should not try to start cdn', () => {
+        expect(res.stderr).to.not.contain('you are running e2e tests');
+        expect(res.stderr).to.not.contain('cdn');
+      });
+
       it('should work load jest configuration and work with css', () => {
         expect(res.stderr).to.not.contain('should work with css');
       });

packages/jest-yoshi-preset/jest-preset.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const globby = require('globby');
 const { envs } = require('./constants');
+const globs = require('../yoshi-config/globs');
 
 module.exports = {
   globalSetup: require.resolve('jest-environment-yoshi-puppeteer/globalSetup'),
@@ -28,7 +29,7 @@ module.exports = {
       {
         displayName: 'e2e',
         testEnvironment: require.resolve('jest-environment-yoshi-puppeteer'),
-        testMatch: ['<rootDir>/test/e2e/**/*.spec.(ts|js){,x}'],
+        testMatch: [`<rootDir>/${globs.puppeteer}`],
         setupFiles: [
           require.resolve(
             'jest-environment-yoshi-bootstrap/environment-setup.js',

packages/yoshi/config/protractor.conf.js
@@ -25,7 +25,7 @@ const onPrepare = (userConf && userConf.onPrepare) || ld.noop;
 const merged = ld.mergeWith(
   {
     framework: 'jasmine',
-    specs: [globs.e2e],
+    specs: [globs.protractor],
     exclude: [],
     directConnect: true,
 

packages/yoshi/src/commands/test.js
@@ -87,7 +87,7 @@ module.exports = runner.command(
 
     if (cliArgs.mocha) {
       if (shouldRunPuppeteer) {
-        specsPattern.push(globs.e2e);
+        specsPattern.push(globs.protractor);
         await bootstrapCdn();
       }
 
@@ -175,7 +175,7 @@ module.exports = runner.command(
     }
 
     if (cliArgs.jest) {
-      if (!shouldWatch) {
+      if (!shouldWatch && hasE2ETests()) {
         await bootstrapCdn();
       }
 

commit 2596f12b2ef7cc9a0b425d821a938f02d049f4b1

Fix puppeteer cdn issue (#787)
### 🔦 Summary
Fixes an issue introduced by #775. In a puppeteer setup, the cdn is not started because we are not checking for the existence of puppeteer e2e test files.

### 🗺️ Test Plan
Added a new simple test for testing with puppeteer. Should give us a basic idea if we break something with puppeteer.

packages/yoshi-config/globs.js
@@ -16,8 +16,8 @@ module.exports = {
   statics,
   babel: [path.join(base, '**', '*.js{,x}'), 'index.js'],
   specs: `${base}/**/*.+(spec|it).+(js|ts){,x}`,
-  protractor: `${test}/**/*.e2e.{js,ts}`,
-  puppeteer: `${test}/e2e/**/*.spec.(ts|js){,x}`,
+  protractor: `${test}/**/*.e2e.+(js|ts)`,
+  puppeteer: `${test}/e2e/**/*.spec.+(ts|js){,x}`,
   testFilesWatch: [path.join(base, '**', '*.(ts|js){,x}'), 'index.js'],
   singleModule: {
     clientDist: statics,

packages/yoshi-helpers/queries.js
@@ -71,7 +71,10 @@ module.exports.shouldRunLess = () => {
 };
 
 module.exports.hasE2ETests = () => {
-  return glob.sync(globs.protractor).length > 0;
+  return (
+    glob.sync(globs.protractor).length > 0 ||
+    glob.sync(globs.puppeteer).length > 0
+  );
 };
 
 module.exports.hasProtractorConfigFile = () => {

packages/yoshi/test/test.spec.js
@@ -404,6 +404,59 @@ describe('Aggregator: Test', () => {
       );
       test.teardown();
     });
+
+    describe('puppeteer environment', () => {
+      it('should pass with passing e2e tests', () => {
+        const cdnPort = 3200;
+        const serverPort = 3100;
+        const e2eTestSampleText = 'Hello World!';
+
+        const test = tp.create();
+        const res = test
+          .setup({
+            'package.json': fx.packageJson(
+              {
+                servers: {
+                  cdn: {
+                    port: cdnPort,
+                  },
+                },
+              },
+              {},
+              {
+                jest: {
+                  preset: 'jest-yoshi-preset',
+                },
+              },
+            ),
+            'index.js': `
+              const http = require('http');
+                const server = http.createServer((req, res) => {
+                const response = "<html><body>${e2eTestSampleText}</body></html>";
+                res.end(response);
+              });
+              server.listen(process.env.PORT);
+            `,
+            'jest-yoshi.config.js': `
+              module.exports = {
+                server: {
+                  command: 'node index.js',
+                  port: ${serverPort},
+                },
+              };
+            `,
+            'test/e2e/some.e2e.spec.js': `
+              it('should succeed', async () => {
+                await page.goto('http://localhost:${serverPort}');
+                expect(await page.$eval('body', e => e.innerText)).toEqual('${e2eTestSampleText}');
+              });
+            `,
+          })
+          .execute('test', ['--jest']);
+
+        expect(res.code).to.equal(0);
+      });
+    });
   });
 
   describe('--mocha', () => {