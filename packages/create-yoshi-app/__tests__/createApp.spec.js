3d5cc254ea691615069bb363a59e62ac3cf668f4
Yaniv Efraim <yaniv.efraim@gmail.com>
2018-11-19 12:54:15 +0200

    mock `verifyRegistry` for unit tests (#814)
    * mock verifyRegistry for unit tests
    
    * return undefined instead of {}

--- packages/create-yoshi-app/__tests__/createApp.spec.js
+++ packages/create-yoshi-app/__tests__/createApp.spec.js
@@ -7,6 +7,7 @@ const { gitInit } = require('../src/utils');
 const fs = require('fs-extra');
 
 jest.mock('../src/runPrompt');
+jest.mock('../src/verifyRegistry');
 jest.mock('../src/utils', () => ({
   ...require.requireActual('../src/utils'),
   install: jest.fn(),
@@ -28,6 +29,7 @@ afterEach(() => {
 test('it should generate a git repo', async () => {
   const tempDir = tempy.directory();
   require('../src/runPrompt').mockReturnValue(minimalTemplateAnswers());
+  require('../src/verifyRegistry').mockReturnValue(undefined);
   await createApp(tempDir);
 
   expect(() => {
@@ -40,6 +42,7 @@ test('it should generate a git repo', async () => {
 
 test('it should not create a git repo if the target directory is contained in a git repo', async () => {
   require('../src/runPrompt').mockReturnValue(minimalTemplateAnswers());
+  require('../src/verifyRegistry').mockReturnValue(undefined);
 
   const tempDir = tempy.directory();
   const projectDir = path.join(tempDir, 'project');
