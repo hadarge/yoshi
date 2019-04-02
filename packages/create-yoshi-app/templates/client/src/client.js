85e09fb8fa9ef49f0ea4ffcebdc2e12c068bb818
Yael Hermon <yaelhe@wix.com>
2019-04-02 10:57:44 +0300

    create-yoshi-app: add sentry and fedops to client and fullstack apps (#967)

--- packages/create-yoshi-app/templates/client/src/client.js
+++ packages/create-yoshi-app/templates/client/src/client.js
@@ -5,11 +5,18 @@ import { I18nextProvider } from 'react-i18next';
 import { wixAxiosConfig } from '@wix/wix-axios-config';
 import App from './components/App';
 import i18n from './i18n';
+import { create as createFedopsLogger } from '@wix/fedops-logger';
 
 const locale = window.__LOCALE__;
 const baseURL = window.__BASEURL__;
 
 wixAxiosConfig(axios, { baseURL });
+
+const fedopsLogger = createFedopsLogger('{%projectName%}');
+
+// Move the following `appLoaded()` call to the point where your app has fully loaded.
+// See https://github.com/wix-private/fed-infra/blob/master/fedops/fedops-logger/README.md
+fedopsLogger.appLoaded();
 
 ReactDOM.render(
   <I18nextProvider i18n={i18n(locale)}>
