08090316915c9264344a7043f3a0555a5d8089b7
Ronen Amiel <ronena@wix.com>
2019-04-07 11:18:44 +0300

    Adjust internal `createVersion` script to generate versioned docs (#1212)

--- scripts/createVersion.js
+++ scripts/createVersion.js
@@ -1,13 +1,11 @@
-process.on('unhandledRejection', error => {
-  throw error;
-});
-
 const path = require('path');
-const cp = require('child_process');
+const execa = require('execa');
 const chalk = require('chalk');
+const semver = require('semver');
 const prompts = require('prompts');
 const pkg = require('../package.json');
 
+const websiteDirectory = path.resolve(__dirname, '../website');
 const lernaPath = path.resolve(__dirname, '../node_modules/.bin/lerna');
 
 // resets the console
@@ -16,41 +14,68 @@ process.stdout.write('\x1Bc');
 console.log(chalk.underline(`starting the release process for ${pkg.name}`));
 console.log();
 
-prompts({
-  type: 'confirm',
-  name: 'value',
-  initial: true,
-  message: 'Did you remember to update changelog with the new version?',
-}).then(({ value }) => {
-  if (!value) {
-    console.log();
-    console.log(chalk.cyan('So do it now 👇'));
-    console.log();
-    console.log(path.resolve(__dirname, '../CHANGELOG.md'));
-    console.log();
-    console.log(chalk.red('Release aborted'));
-  } else {
-    try {
-      cp.execSync(`${lernaPath} publish --exact --skip-npm`, {
-        stdio: 'inherit',
-      });
+Promise.resolve()
+  // Ask to create changelog
+  .then(() =>
+    prompts({
+      type: 'confirm',
+      name: 'value',
+      initial: true,
+      message: 'Did you remember to update changelog with the new version?',
+    }),
+  )
+  .then(({ value }) => {
+    if (!value) {
+      console.log();
+      console.log(chalk.cyan('So do it now 👇'));
+      console.log();
+      console.log(path.resolve(__dirname, '../CHANGELOG.md'));
+      console.log();
+      console.log(chalk.red('Release aborted'));
 
-      console.log();
-      console.log(chalk.green('Release was created locally'));
-      console.log();
-      console.log('Please push your changes to origin');
-      console.log();
-      console.log(chalk.cyan('git push --follow-tags'));
-      console.log();
-      console.log('Head over to the CI and wait for yoshi build to pass 👇');
-      console.log();
-      console.log(
-        chalk.cyan(
-          'http://ci.dev.wix/viewType.html?buildTypeId=Wix_Angular_WixHaste_HastePresetYoshi',
-        ),
-      );
-    } catch (error) {
-      throw error;
+      process.exit(1);
     }
-  }
-});
+  })
+  // Create new tag/bump versions in `package.json` files
+  .then(() => {
+    execa.shellSync(`${lernaPath} publish --exact --skip-npm`, {
+      stdio: 'inherit',
+    });
+  })
+  // Update docs according to new version
+  .then(() => {
+    const lernaJson = require('../lerna.json');
+    const majorVersion = semver.major(lernaJson);
+
+    execa.shellSync(`npm run version "${majorVersion}.x"`, {
+      cwd: websiteDirectory,
+      stdio: 'inherit',
+    });
+
+    execa.shellSync(
+      `git commit -a -m "documentation for version ${majorVersion}"`,
+    );
+  })
+  .then(() => {
+    console.log();
+    console.log(chalk.green('Release was created locally'));
+    console.log();
+    console.log('Please push your changes to origin');
+    console.log();
+    console.log(chalk.cyan('git push --follow-tags'));
+    console.log();
+    console.log('Head over to the CI and wait for yoshi build to pass 👇');
+    console.log();
+    console.log(
+      chalk.cyan(
+        'http://ci.dev.wix/viewType.html?buildTypeId=Wix_Angular_WixHaste_HastePresetYoshi',
+      ),
+    );
+  })
+  .catch(error => {
+    console.log(chalk.red('Version release failed.'));
+    console.log(chalk.red(error));
+    console.log();
+
+    process.exit(1);
+  });
