23d6c5c19b33d5f5bd33e37cded82be202877c7d
Arnoldas Šeščila <arnoldass@wix.com>
2019-05-05 17:46:59 +0300

    Add spec-setup with react-testing-library/clean-after-each (#1267)
    * Add spec-setup with react-testing-library/clean-after-each
    
    * Move e2e test files back to it's original location

--- packages/create-yoshi-app/templates/fullstack/javascript/src/components/App/App.spec.js
+++ packages/create-yoshi-app/templates/fullstack/javascript/src/components/App/App.spec.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { render, cleanup } from 'react-testing-library';
+import { render } from 'react-testing-library';
 import { I18nextProvider } from 'react-i18next';
 import App from './App';
 import i18n from '../__mocks__/i18n';
 
 describe('App', () => {
-  afterEach(cleanup);
-
   it('renders a title correctly', () => {
     const { getByTestId } = render(
       <I18nextProvider i18n={i18n}>
