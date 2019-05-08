05b139842c45120693f789dd0f32aadd12ec11a7
Gil Eckstein <gileck@wix.com>
2019-05-08 16:51:05 +0300

    fix OOI e2e tests (#1281)

--- packages/create-yoshi-app/templates/out-of-iframe/javascript/test/e2e/editorApp.spec.js
+++ packages/create-yoshi-app/templates/out-of-iframe/javascript/test/e2e/editorApp.spec.js
@@ -1,6 +1,6 @@
-describe.skip('React application', () => {
+describe('React application', () => {
   it('should display title', async () => {
-    await page.goto('https://localhost:3100/editorExampleWidget');
+    await page.goto('https://localhost:3100/editorApp');
     await page.waitForSelector('h2');
 
     expect(await page.$eval('h2', e => e.innerText)).toEqual('Hello World!');
