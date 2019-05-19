73adb49ab107d3fef5898b43f9d466e3813c998b
Artem Yavorsky <info@yavorsky.org>
2019-05-19 16:33:36 +0300

    Update fullstack/client templates to use lazy-loaded Intro. (#1284)
    * Update fullstack-babel template to use lazy-loaded Intro.
    
    * Update locales.
    
    * Update fullstack typescript project template
    
    * Add client templates
    
    * Update client/ts configs.
    
    * Use @latest tag for dynamic import
    
    * Update ts templates

--- packages/create-yoshi-app/templates/fullstack/javascript/src/components/App/App.js
+++ packages/create-yoshi-app/templates/fullstack/javascript/src/components/App/App.js
@@ -1,21 +1,42 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { translate } from 'react-i18next';
 import s from './App.scss';
-import PropTypes from 'prop-types';
+
+/* <-- To remove demo stuff just copy-paste:
+  \{?/\*\s?<--([\n\n]|.)*?-->\s?\*\/\}?
+  to your search input with RegExp enabled and remove everything matched.
+--> */
 
 class App extends React.Component {
   static propTypes = {
     t: PropTypes.func,
   };
 
+  /* <-- Feel free to remove this lifecycle hook */
+  state = {};
+  async componentDidMount() {
+    await import(
+      /* webpackIgnore: true */
+      // eslint-disable-next-line import/no-unresolved
+      'https://unpkg.com/yoshi-template-intro@latest'
+    );
+    const { default: TemplateIntro } = window.TemplateIntro;
+    this.setState({ TemplateIntro });
+  } /* --> */
+
   render() {
     const { t } = this.props;
+
     return (
       <div className={s.root}>
-        <div className={s.header}>
-          <h2 data-testid="app-title">{t('app.title')}</h2>
-        </div>
-        <p className={s.intro}>{t('app.intro')}</p>
+        {/* <-- Feel free to remove h2 and TemplateIntro */}
+        <h2 className={s.title} data-testid="app-title">
+          {t('app.title')}
+        </h2>
+        {this.state.TemplateIntro &&
+          React.createElement(this.state.TemplateIntro)}
+        {/* --> */}
       </div>
     );
   }
--- packages/create-yoshi-app/templates/fullstack/typescript/src/components/App/App.tsx
+++ packages/create-yoshi-app/templates/fullstack/typescript/src/components/App/App.tsx
@@ -2,18 +2,39 @@ import * as React from 'react';
 import { translate, InjectedTranslateProps } from 'react-i18next';
 import * as s from './App.scss';
 
+/* <-- To remove demo stuff just copy-paste:
+  \{?/\*\s?<--([\n\n]|.)*?-->\s?\*\/\}?
+  to your search input with RegExp enabled and remove everything matched.
+--> */
+
 interface AppProps extends InjectedTranslateProps {}
 
 class App extends React.Component<AppProps> {
+  /* <-- Feel free to remove this lifecycle hook and state */
+  state = {
+    TemplateIntro: () => null,
+  };
+  async componentDidMount() {
+    await import(
+      /* webpackIgnore: true */
+      // @ts-ignore
+      'https://unpkg.com/yoshi-template-intro@latest'
+    );
+    const { default: TemplateIntro } = (window as any).TemplateIntro;
+    this.setState({ TemplateIntro });
+  } /* --> */
+
   render() {
     const { t } = this.props;
 
     return (
       <div className={s.root}>
-        <div className={s.header}>
-          <h2 data-testid="app-title">{t('app.title')}</h2>
-        </div>
-        <p className={s.intro}>{t('app.intro')}</p>
+        {/* <-- Feel free to remove h2 and TemplateIntro */}
+        <h2 className={s.title} data-testid="app-title">
+          {t('app.title')}
+        </h2>
+        <this.state.TemplateIntro />
+        {/* --> */}
       </div>
     );
   }
