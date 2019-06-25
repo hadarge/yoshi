cfc03a773d9665bcd08690962dcf960d1431b497
Ronen Amiel <ronen.amiel@gmail.com>
2019-06-25 12:36:07 +0300

    run wnpm-ci prepareForRelase only in master CI (#1399)

--- packages/yoshi/src/commands/release-monorepo.js
+++ packages/yoshi/src/commands/release-monorepo.js
@@ -16,31 +16,31 @@ const inTeamCity = checkInTeamCity();
 const inPRTeamCity = checkInPRTeamCity();
 
 module.exports = async () => {
-  const [, libs] = await splitPackagesPromise;
-
-  // Patch libraries' `package.json` main field to point to `dist`
-  await Promise.all(
-    libs.map(async lib => {
-      const packageJsonPath = path.join(lib.location, 'package.json');
-      const json = await fs.readJSON(packageJsonPath);
-
-      // Point to js version of the file in the `dist` directory
-      json.main = path.join('dist', json.main.replace('.ts', '.js'));
-
-      fs.writeFileSync(packageJsonPath, JSON.stringify(json, null, 2));
-    }),
-  );
-
-  await Promise.all(
-    libs.map(lib => {
-      return wnpm.prepareForRelease({ shouldBumpMinor, cwd: lib.location });
-    }),
-  );
-
-  // This part is inconsistent with how non-monorepo apps work:
-  // Here we publish packages as part of `yoshi release` while in most apps
-  // CI does the publishinng
   if (inTeamCity && !inPRTeamCity) {
+    const [, libs] = await splitPackagesPromise;
+
+    // Patch libraries' `package.json` main field to point to `dist`
+    await Promise.all(
+      libs.map(async lib => {
+        const packageJsonPath = path.join(lib.location, 'package.json');
+        const json = await fs.readJSON(packageJsonPath);
+
+        // Point to js version of the file in the `dist` directory
+        json.main = path.join('dist', json.main.replace('.ts', '.js'));
+
+        fs.writeFileSync(packageJsonPath, JSON.stringify(json, null, 2));
+      }),
+    );
+
+    await Promise.all(
+      libs.map(lib => {
+        return wnpm.prepareForRelease({ shouldBumpMinor, cwd: lib.location });
+      }),
+    );
+
+    // This part is inconsistent with how non-monorepo apps work:
+    // Here we publish packages as part of `yoshi release` while in most apps
+    // CI does the publishinng
     await Promise.all(
       libs.map(lib => {
         console.log(`Publishing ${lib.name}...`);
