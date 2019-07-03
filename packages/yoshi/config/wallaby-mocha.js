a745fb9ae37930d56a1445bf574ad8e911fc3e5d
Ronen Amiel <ronen.amiel@gmail.com>
2019-07-03 14:16:00 +0300

    Adjust wallaby to version 4 changes (#1437)

--- packages/yoshi/config/wallaby-mocha.js
+++ packages/yoshi/config/wallaby-mocha.js
@@ -1,22 +1,27 @@
 module.exports = function(wallaby) {
   const wallabyCommon = require('./wallaby-common')(wallaby);
+
   wallabyCommon.tests.unshift(
     { pattern: 'test/**/*.+(spec|it).[j|t]s' },
     { pattern: 'test/**/*.+(spec|it).[j|t]sx' },
   );
+
   wallabyCommon.compilers = {
     '**/*.js{,x}': wallaby.compilers.babel({
       babel: require('@babel/core'),
-      babelrc: true,
-      plugins: [require.resolve('@babel/plugin-transform-modules-commonjs')],
+      babelrc: false,
+      plugins: [require.resolve('babel-preset-yoshi')],
     }),
   };
+
   wallabyCommon.testFramework = 'mocha';
+
   wallabyCommon.setup = () => {
     const mocha = wallaby.testFramework;
     mocha.timeout(30000);
     process.env.IN_WALLABY = true;
     require('yoshi/config/test-setup'); // eslint-disable-line import/no-unresolved
   };
+
   return wallabyCommon;
 };
