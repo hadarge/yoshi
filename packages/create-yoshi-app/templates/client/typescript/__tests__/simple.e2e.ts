7aa5b691d533bfd86e8dce52fd980720779a1795
Yaniv Efraim <yaniv.efraim@gmail.com>
2019-07-31 11:32:25 +0300

    Remove redundant `wait-for-selector` from generated code (#1472)

--- packages/create-yoshi-app/templates/client/typescript/__tests__/simple.e2e.ts
+++ packages/create-yoshi-app/templates/client/typescript/__tests__/simple.e2e.ts
@@ -1,7 +1,6 @@
 describe('React application', () => {
   it('should display title', async () => {
     await page.goto('http://localhost:3100');
-    await page.waitForSelector('h2');
 
     expect(
       await page.$eval('h2', (e: HTMLHeadingElement) => e.innerText),
