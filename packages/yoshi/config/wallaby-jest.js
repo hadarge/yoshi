10116bc9b218d0623b8bcad9e2705f381ac80742
Ran Yitzhaki <ranyitz@users.noreply.github.com>
2019-11-13 17:48:24 +0200

    correct the path of yoshi's jest.config.js (#1663)

--- packages/yoshi/config/wallaby-jest.js
+++ packages/yoshi/config/wallaby-jest.js
@@ -10,7 +10,7 @@ module.exports = function(wallaby) {
   };
   wallabyCommon.testFramework = 'jest';
   wallabyCommon.setup = () => {
-    let jestConfig = require('yoshi/config/jest.config.js'); // eslint-disable-line import/no-unresolved
+    let jestConfig = require('yoshi-flow-legacy/config/jest.config.js'); // eslint-disable-line import/no-unresolved
     if (jestConfig.preset === 'jest-yoshi-preset') {
       const jestYoshiPreset = require('jest-yoshi-preset/jest-preset'); // eslint-disable-line import/no-unresolved
       jestConfig = jestYoshiPreset.projects.find(
